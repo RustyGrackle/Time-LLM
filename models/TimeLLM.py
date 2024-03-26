a502e5cebb9a9de5a260ab3611775679ef9d3b45
Siddharth Mishra <98332290+RustyGrackle@users.noreply.github.com>
2024-03-26 19:33:14 +0530

    Update TimeLLM.py

--- models/TimeLLM.py
+++ models/TimeLLM.py
@@ -41,8 +41,8 @@ class Model(nn.Module):
         self.patch_len = configs.patch_len
         self.stride = configs.stride
 
-        self.llama_config = LlamaConfig.from_pretrained('/mnt/alps/modelhub/pretrained_model/LLaMA/7B_hf/')
-        # self.llama_config = LlamaConfig.from_pretrained('huggyllama/llama-7b')
+        # self.llama_config = LlamaConfig.from_pretrained('/mnt/alps/modelhub/pretrained_model/LLaMA/7B_hf/')
+        self.llama_config = LlamaConfig.from_pretrained('huggyllama/llama-7b')
         self.llama_config.num_hidden_layers = configs.llm_layers
         self.llama_config.output_attentions = True
         self.llama_config.output_hidden_states = True
